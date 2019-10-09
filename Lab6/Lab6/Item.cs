975d1e070e2ab6ed56fd91a3091d9adf3fc24165
sejun <sejun425@gmail.com>
2019-10-09 20:58:12 +0900

    [Lab6] first

---
+++ Lab6/Lab6/Item.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab6
+{
+    public class Item
+    {
+        private EType mType;
+        private double mWeight;
+        private double mVolume;
+        private bool mIsToxicWaste;
+
+        public EType Type
+        {
+            get { return mType; }
+        }
+        public double Weight
+        {
+            get { return mWeight; }
+        }
+        public double Volume
+        {
+            get { return mVolume; }
+        }
+        public bool IsToxicWaste
+        {
+            get { return mIsToxicWaste; }
+        }
+
+        public Item(EType type, double weight, double volume, bool isToxicWaste)
+        {
+            mType = type;
+            mWeight = weight;
+            mVolume = volume;
+            mIsToxicWaste = isToxicWaste;
+        }
+    }
+}
---
+++ Lab6/Lab6/Recyclebot.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections.Generic;
+
+namespace Lab6
+{
+    class Recyclebot
+    {
+        private List<Item> mRecycleItems;
+        private List<Item> mNonRecycleItems;
+
+        public Recyclebot()
+        {
+            mRecycleItems = new List<Item>();
+            mNonRecycleItems = new List<Item>();
+        }
+        public List<Item> RecycleItems
+        {
+            get { return mRecycleItems; }
+        }
+        public List<Item> NonRecycleItems
+        {
+            get { return mNonRecycleItems; }
+        }
+
+        public void Add(Item item)
+        {
+            if (item.Type != EType.Paper && item.Type != EType.Furniture && item.Type != EType.Electronics)
+            {
+                mRecycleItems.Add(item);
+            }
+            else
+            {
+                if (item.Weight >= 2 && item.Weight < 5)
+                {
+                    mRecycleItems.Add(item);
+                }
+                else
+                {
+                    mNonRecycleItems.Add(item);
+                }
+            }
+
+        }
+        public List<Item> Dump()
+        {
+            List<Item> dumpList = new List<Item>();
+            for (int i = 0; i < mNonRecycleItems.Count; i++)
+            {
+                bool bFirstConditionalProposal;
+                Item item = mNonRecycleItems[i];
+
+                // 첫 p->q 조건명제
+                if (item.Volume == 10 || item.Volume == 11 || item.Volume == 15)
+                {
+                    bFirstConditionalProposal = true;
+                }
+                else
+                {
+                    if (item.IsToxicWaste == true)
+                    {
+                        bFirstConditionalProposal = true;
+                    }
+                    else
+                    {
+                        bFirstConditionalProposal = false;
+                    }
+                }
+
+                // 두번째 q->r 조건명제
+                if (bFirstConditionalProposal == false)
+                {
+                    dumpList.Add(item);
+                }
+                else
+                {
+                    if (item.Type == EType.Furniture || item.Type == EType.Electronics)
+                    {
+                        dumpList.Add(item);
+                    }
+                }
+            }
+            return dumpList;
+        }
+    }
+}
