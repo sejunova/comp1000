5698bf16772b67d7edd562f6688a1dc83497f719
sejun <sejun425@gmail.com>
2019-11-10 18:43:43 +0900

    [ass3]

--- Assignment3/Assignment3/StepMaker.cs
+++ Assignment3/Assignment3/StepMaker.cs
@@ -11,11 +11,16 @@ namespace Assignment3
             List<int> allSteps = steps.ToList();
             Dictionary<Tuple<int, int>, int> exitPoints = new Dictionary<Tuple<int, int>, int>();
 
-            MakeStepsRecursive(allSteps, noise, 0, exitPoints);
+            for (int i = 0; i < allSteps.Count - 1; i++)
+            {
+                Tuple<int, int> key = new Tuple<int, int>(allSteps[i], allSteps[i + 1]);
+                exitPoints[key] = 0;
+            }
+            MakeStepsRecursive(allSteps, noise, exitPoints);
             return allSteps;
         }
 
-        private static void MakeStepsRecursive(List<int> allSteps, INoise noise, int level, Dictionary<Tuple<int, int>, int> exitPoints)
+        private static void MakeStepsRecursive(List<int> allSteps, INoise noise, Dictionary<Tuple<int, int>, int> exitPoints)
         {
             List<int> steps = new List<int>(allSteps);
 
@@ -25,18 +30,7 @@ namespace Assignment3
                 if (Math.Abs(steps[i] - steps[i + 1]) > 10)
                 {
                     Tuple<int, int> key = new Tuple<int, int>(steps[i], steps[i + 1]);
-                    if (level != 0)
-                    {
-                        if (exitPoints.ContainsKey(key))
-                        {
-                            level = exitPoints[key] + 1;
-                        }
-                        else
-                        {
-                            exitPoints[key] = -1;
-                            level = 0;
-                        }
-                    }
+                    int level = exitPoints[key];
 
                     int p1 = (int)((1 - 0.2) * steps[i] + 0.2 * steps[i + 1]) + noise.GetNext(level);
                     int p2 = (int)((1 - 0.4) * steps[i] + 0.4 * steps[i + 1]) + noise.GetNext(level);
@@ -48,12 +42,12 @@ namespace Assignment3
                     allSteps.Insert(insertIndex++, p3);
                     allSteps.Insert(insertIndex++, p4);
 
-                    exitPoints[new Tuple<int, int>(steps[i], p1)] = level;
-                    exitPoints[new Tuple<int, int>(p1, p2)] = level;
-                    exitPoints[new Tuple<int, int>(p2, p3)] = level;
-                    exitPoints[new Tuple<int, int>(p3, p4)] = level;
-                    exitPoints[new Tuple<int, int>(p4, steps[i + 1])] = level;
-                    MakeStepsRecursive(allSteps, noise, level + 1, exitPoints);
+                    exitPoints[new Tuple<int, int>(steps[i], p1)] = level + 1;
+                    exitPoints[new Tuple<int, int>(p1, p2)] = level + 1;
+                    exitPoints[new Tuple<int, int>(p2, p3)] = level + 1;
+                    exitPoints[new Tuple<int, int>(p3, p4)] = level + 1;
+                    exitPoints[new Tuple<int, int>(p4, steps[i + 1])] = level + 1;
+                    MakeStepsRecursive(allSteps, noise, exitPoints);
                     break;
                 }
                 insertIndex++;
